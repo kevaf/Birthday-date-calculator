8b8cb1ba4bb84ac15dcfd41a57e844fa1de708ce
kevaf <kevinkipyegon20@gmail.com>
2021-07-17 23:01:38 +0300

    added javascript

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-var name = document.getElementById('name').value
+
+function akanFunction(){
+    
+const days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const mNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const fNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
 var dob = parseInt(document.getElementById('dob').value);
 var mob = parseInt(document.getElementById('mob').value);
 var yob = (document.getElementById('yob').value);
-var yy = parseInt(yob.slice(2,4));
 
-var century = toString(yob);
-var cc = parseInt(century.slice(0,3));
+var a = Math.floor((14 - mob) / 12);
+var y = yob - a;
+var m = mob + 12 * a - 2;
+
+var date = (day + y + Math.floor(y / 4) - Math.floor(y / 100) 
+            + Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
 
 
 
-var date = (((cc/4)-2*cc-1)+(5*yy/4)+(26*(mob+1)/10)+dob)%7;
+
+document.getElementById("output").innerHTML=(date);
+
+}
